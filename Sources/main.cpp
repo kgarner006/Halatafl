9480a2d39c8b3bf9c32c2bd7c04dd593e3f2e493
kgarner006 <46944124+kgarner006@users.noreply.github.com>
2020-12-04 04:31:33 -0500

    Create main.cpp

---
+++ Sources/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "manager.h"
+
+using namespace std;
+
+// This function will call the manageGame function with an input stream.
+int main(int argc, char** argv)
+{
+    //// Call the manageGame function and end the program when manageGame is done.
+	if (argc > 1)
+	{
+	    ifstream in (argv[1]);
+
+        manageGame(in);
+	}
+	else
+        manageGame(cin);
+
+
+    return 0;
+}
