7afd7c82f1e623eaffc1e86c96733aaa19a13f36
kgarner006 <46944124+kgarner006@users.noreply.github.com>
2020-12-09 10:22:54 -0500

    Update sheep.cpp

--- Sources/sheep.cpp
+++ Sources/sheep.cpp
@@ -1,6 +1,7 @@
 #include "sheep.h"
 #include "board.h"
 #include <iostream>
+#include <ctime>
 
 using namespace std;
 
@@ -19,15 +20,21 @@ int sheep::numberOfSheep(char gameBoard[7][7])
 }
 
 
-//// This function prompts and reads the user's input, tests whether or not the new position they entered
-//// is legal, and if it is, the new position in the gameBoard array parameter gets changed to 'S'.
+/*/// This function prompts and reads the user's input, tests whether or not the new position they entered
+//// is legal, and if it is, the new position in the "board.h" public board array gets changed to 'S'.
 //// If the new position the user entered was illegal, then it will continually ask the user
-//// to enter a legal position until the user does so.
-void sheep::userInput(char gameBoard[7][7], istream& in)
+//// to enter a legal position until the user does so. If the user is playing blitz (speed) Halatafl,
+//// then their turn is timed (recorded in 'duration'). If they do not make a move in 20 seconds,
+//// the function returns false. Otherwise, the function will return true.*/
+bool sheep::userInput(char gameBoard[7][7], std::istream& in, bool speedHalatafl, double& duration)
 {
     ///// Create 5 variables to read the input of the user, initial position - moving position.
     char oldPos = 'x', dash = '-', newPos = 'y';
     int oldPosition = 0, newPosition = 0;
+
+    // Timer Variables for Blitz (Speed) Halatafl
+    clock_t start;
+    duration = 0;
 
     ///// Create an array to match the letters that represent the game board columns with
     ///// the two-dimensional gameBoard array.
@@ -38,7 +45,51 @@ void sheep::userInput(char gameBoard[7][7], istream& in)
 
     ///// Prompt user for input and read data into variables.
     cout << "\nYour move? ";
+    if (speedHalatafl) {
+        start = clock();
+    }
     in >> oldPos >> oldPosition >> dash >> newPos >> newPosition;
+    if (speedHalatafl) {
+        duration = (clock() - start) / (double) CLOCKS_PER_SEC;
+        if (duration > 20)
+            return false;
+    }
+
+    int validLetters = 0;
+    int validNumbers = 0;
+    for (int x = 0; x < 7; x++) {
+        if (oldPos == letterArray[x])
+            validLetters++;
+        if (newPos == letterArray[x])
+            validLetters++;
+        if (oldPosition == numberArray[x])
+            validNumbers++;
+        if (newPosition == numberArray[x])
+            validNumbers++;
+    }
+    while (validLetters != 2 && validNumbers != 2)
+    {
+        if (speedHalatafl) {
+            duration = (clock() - start) / (double) CLOCKS_PER_SEC;
+            if (duration > 20)
+                return false;
+        }
+        validLetters = 0;
+        validNumbers = 0;
+        cout << "\nThat is not a legal move.\nYour move? ";
+        in >> oldPos >> oldPosition >> dash >> newPos >> newPosition;
+        for (int x = 0; x < 7; x++) {
+            if (oldPos == letterArray[x])
+                validLetters++;
+            if (newPos == letterArray[x])
+                validLetters++;
+            if (oldPosition == numberArray[x])
+                validNumbers++;
+            if (newPosition == numberArray[x])
+                validNumbers++;
+        }
+    }
+
     ///// Match oldPos, newPos, oldPosition, and newPosition with the letter and number arrays,
     ///// and store those values in new variables
     int columnOld = 0, rowOld = 0, columnNew = 0, rowNew = 0;
@@ -55,6 +106,11 @@ void sheep::userInput(char gameBoard[7][7], istream& in)
     ///// Check the new board array position to see if it's a legal move
     ////// Loop until user enters proper position
     while (gameBoard[rowOld][columnOld] != 'S') {
+        if (speedHalatafl) {
+            duration = (clock() - start) / (double) CLOCKS_PER_SEC;
+            if (duration > 20)
+                return false;
+        }
         cout << "\nThat is not a legal move.\nYour move? ";
         in >> oldPos >> oldPosition >> dash >> newPos >> newPosition;
         for (int x = 0; x < 7; x++) {
@@ -69,6 +125,11 @@ void sheep::userInput(char gameBoard[7][7], istream& in)
         }
     }
     while ((columnNew < 0 || columnNew > 6) || (rowNew < 0 || rowNew > 6)) {
+        if (speedHalatafl) {
+            duration = (clock() - start) / (double) CLOCKS_PER_SEC;
+            if (duration > 20)
+                return false;
+        }
         cout << "\nThat is not a legal move.\nYour move? ";
         in >> oldPos >> oldPosition >> dash >> newPos >> newPosition;
         for (int x = 0; x < 7; x++) {
@@ -85,7 +146,11 @@ void sheep::userInput(char gameBoard[7][7], istream& in)
 
     while ((gameBoard[rowNew][columnNew] != '.') ||
            (((rowNew == rowOld + 1) && (columnNew == columnOld)) || ((rowNew == rowOld + 1) && (columnNew == columnOld + 1)) || ((rowNew == rowOld + 1) && (columnNew == columnOld - 1)) || ((rowNew == rowOld - 1) && (columnNew == columnOld - 1)) || ((rowNew == rowOld - 1) && (columnNew == columnOld + 1))) || ((rowNew > rowOld + 1) || (rowNew < rowOld - 1) || (columnNew > columnOld + 1) || (columnNew < columnOld - 1))) {
-
+        if (speedHalatafl) {
+            duration = (clock() - start) / (double) CLOCKS_PER_SEC;
+            if (duration > 20)
+                return false;
+        }
         cout << "\nThat is not a legal move.\nYour move? ";
         in >> oldPos >> oldPosition >> dash >> newPos >> newPosition;
         for (int x = 0; x < 7; x++) {
@@ -104,4 +169,6 @@ void sheep::userInput(char gameBoard[7][7], istream& in)
     ////// Set the new board position to 'S' and change the old position to '.'
     gameBoard[rowNew][columnNew] = 'S';
     gameBoard[rowOld][columnOld] = '.';
+
+    return true;
 }
