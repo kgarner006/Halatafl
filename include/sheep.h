6d109d7a8634c8fd3142408abab14e41936086e0
kgarner006 <46944124+kgarner006@users.noreply.github.com>
2020-12-09 10:25:25 -0500

    Update sheep.h

--- include/sheep.h
+++ include/sheep.h
@@ -16,8 +16,10 @@ public:
     /*/// This function prompts and reads the user's input, tests whether or not the new position they entered
     //// is legal, and if it is, the new position in the "board.h" public board array gets changed to 'S'.
     //// If the new position the user entered was illegal, then it will continually ask the user
-    //// to enter a legal position until the user does so.*/
-    void userInput(char gameBoard[7][7], std::istream& in);
+    //// to enter a legal position until the user does so. If the user is playing blitz (speed) Halatafl,
+    //// then their turn is timed (recorded in 'duration'). If they do not make a move in 20 seconds,
+    //// the function returns false. Otherwise, the function will return true.*/
+    bool userInput(char gameBoard[7][7], std::istream& in, bool speedHalatafl, double& duration);
 };
 
 
